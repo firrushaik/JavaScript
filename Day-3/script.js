18bfeb61a6628fa1b510bc933ac84876b9778833
firrushaik <shaikfirdose7799@gmail.com>
2025-11-01 23:00:52 +0530

    Day-3 programs uploaded

--- Day-3/script.js
+++ Day-3/script.js
@@ -1,7 +1,22 @@
-// var a=10;
-// console.log(a);
+var a=10;
+console.log(a)
 
-// let b = 20;
-// concole.log(b)
+if(true){
+// let a = 20;
+// console.log(a); // 20
 
-console
+// var a=20;
+// console.log(a) // re declared var keyword
+
+// var a=30;
+// console.log(a) // multiple times re declared var variable
+
+let a = 30;
+console.log(a)
+
+// let a=40;
+// console.log(a) // only one time we declared the same variable if we intialize that same variable yes we can do that
+
+a = 40;
+console.log(a)
+}
