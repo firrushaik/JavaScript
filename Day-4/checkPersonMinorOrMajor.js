38085ca136bcf8f3ffc1035eeac0184c3724eb03
firrushaik <shaikfirdose7799@gmail.com>
2025-11-03 10:16:56 +0530

    Day-4 programs uploaded

---
+++ Day-4/checkPersonMinorOrMajor.js
@@ -0,0 +1,15 @@
+let age = 16;
+
+/* if(age>18){
+    console.log("You are major")
+}else{
+    console.log("You are minor") // o:p you are minor
+} */
+
+   age=22;
+
+    if(age>18){
+        console.log("you are major");
+    }else{
+        console.log("you are minor")
+    }
