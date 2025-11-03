c087df5af9da6444a52432872516a68141189726
firrushaik <shaikfirdose7799@gmail.com>
2025-11-03 10:24:07 +0530

    Day-4 programs uploaded

---
+++ Day-4/checkPersonEligibleToVoteOrNot.js
@@ -0,0 +1,10 @@
+// If the person age is greater than or equal to 18 then It is eligible to do vote
+
+let age=18;
+//  age=20;
+//  age=10;
+if(age>=18){
+    console.log("Eligible for vote")
+}else{
+    console.log("Not eligible for vote")
+}
