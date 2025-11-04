deca38641dfd3cdf860d6630665f402598d5eaa1
firrushaik <shaikfirdose7799@gmail.com>
2025-11-04 16:02:10 +0530

    Day-5 programs uploaded

---
+++ Day-2/script.js
@@ -0,0 +1,24 @@
+//we can declare same variable multiple time by using var.      
+
+var a=10;
+console.log(a) //10
+
+a=20;
+console.log(a)  //20
+
+var a=30;
+console.log(a)  //30
+
+//we can declare same variable only one time using let.
+
+// let a=10;
+// console.log(a) 
+
+/* a=20;
+console.log(a)  // error
+
+/* var a=30;
+console.log(a) */
+
+let b=10;
+console.log(a)
