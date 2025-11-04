2343f8caa876c7f8f1fc437d4332a41c0a6a11da
firrushaik <shaikfirdose7799@gmail.com>
2025-11-04 16:24:05 +0530

    Day-5 programs uploaded

---
+++ Day-5/checkPrimeOrNot.js
@@ -0,0 +1,18 @@
+
+
+let n = 3;
+let factorsCount=0;
+
+for(let i=1;i<=n;i++){
+    if(n%i==0){
+        factorsCount++;
+    }
+}
+
+if(factorsCount==2){
+    console.log(n + " Is a prime number");
+}else{
+    console.log(n + " Is not a prime number");
+}
+
+
---
+++ Day-5/factorsOfNumbers.js
@@ -0,0 +1,22 @@
+// print factors program:
+
+// let  n = 10;
+
+// for(let i=1;i<=n;i++){
+//     if(n%i==0){
+//         console.log(i);
+//     }
+// }
+
+
+//count factors program:
+
+let  n = 10;
+let factorsCount=0;
+
+for(let i=1;i<=n;i++){
+    if(n%i==0){
+        factorsCount++;
+    }
+}
+console.log(factorsCount);
