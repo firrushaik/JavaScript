2331076c418b26af2dea1498276d3f6b928538f5
firrushaik <shaikfirdose7799@gmail.com>
2025-11-06 23:06:43 +0530

    Day-6 programs uploaded

---
+++ Day-6/script.js
@@ -0,0 +1,39 @@
+let a=10;
+console.log(a);
+
+
+for(let b=1;b<=10;b++){
+    console.log("Hello");
+}
+
+// Do while loop examle:
+
+let b = 20;
+
+do{
+    console.log("hello");
+    b++;
+}while(b<=20);
+
+
+// sum of digits
+
+/* let num = 153;
+
+let sum = 0;
+
+while(num != 0){
+    sum += num % 10;
+
+    num = Math.floor(num /  10);
+}
+
+console.log(sum); */
+
+
+let n = 5;
+
+do{
+    console.log("hiii everyone!");
+    n += 1;
+}while(n<=10)
