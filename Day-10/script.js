5fc97044008ede6e6f3e19536c4dc2ec72f6a21c
firrushaik <shaikfirdose7799@gmail.com>
2025-11-12 23:12:16 +0530

    Day-10 programs uploaded

---
+++ Day-10/script.js
@@ -0,0 +1,7 @@
+
+/* DOM: Document Object Model.
+Window Object -> whenever script file is loaded in the browser will create a new object called window object. */
+
+console.log("Hello");
+console.log(Window);
+
