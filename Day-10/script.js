d1f31e62a0852935e10debe17f256dd5f10f64ab
firrushaik <shaikfirdose7799@gmail.com>
2025-11-13 22:51:27 +0530

    Day-10 programs uploaded

--- Day-10/script.js
+++ Day-10/script.js
@@ -3,5 +3,22 @@
 Window Object -> whenever script file is loaded in the browser will create a new object called window object. */
 
 console.log("Hello");
-console.log(Window);
+// console.log(window);
+// console.log(window.document); // It will give same as it is or html code in console 
+
+// console.dir(window.document); // It will give our code by object format
+
+alert("Welcome to java script!!"); // It will give only alert messeage when we open that page
+
+console.dir(window.document.body);
+
+console.log(document.body.childNodes[5]);
+
+document.body.childNodes[5].innerText =  " On the bulb";
+
+/* Acess the tags using 
+
+document.getElementById();
+document.getElementByClassNmae();
+document.getElementByTagName(); */
 
