5fb4c02a53b3c53bc5937d40148deb9a0df396e1
firrushaik <shaikfirdose7799@gmail.com>
2025-11-26 23:28:08 +0530

    Day-17 programs uploaded

---
+++ Day-17/script.js
@@ -0,0 +1,13 @@
+
+function checkAnagram() {
+      const firstWord = document.getElementById('word1').value.trim().toLowerCase();
+      const secondWord = document.getElementById('word2').value.trim().toLowerCase();
+      if (firstWord.length !== secondWord.length) {
+        document.getElementById("result").textContent = "Not Anagram";
+        return;
+      }else{
+         document.getElementById("result").textContent = "Anagrams";
+         return;
+      }
+      
+    }
