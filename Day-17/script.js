2c5f37c75b0a06cf427e882b2d3960a9717920b2
firrushaik <shaikfirdose7799@gmail.com>
2025-11-27 11:15:16 +0530

    Day-17 programs uploaded

--- Day-17/script.js
+++ Day-17/script.js
@@ -3,7 +3,7 @@ function checkAnagram() {
       const firstWord = document.getElementById('word1').value.trim().toLowerCase();
       const secondWord = document.getElementById('word2').value.trim().toLowerCase();
       if (firstWord.length !== secondWord.length) {
-        document.getElementById("result").textContent = "Not Anagram";
+        document.getElementById("result").textContent = "Not Anagrams";
         return;
       }else{
         let sortedFirstWord = firstWord.split('').sort().join('');
