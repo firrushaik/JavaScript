50559b0116b73649e97ebe353a8a04facdaeb9c6
firrushaik <shaikfirdose7799@gmail.com>
2025-12-03 23:40:21 +0530

    Day-20 programs uploaded

--- Day-20/script.js
+++ Day-20/script.js
@@ -15,9 +15,32 @@
 // console.log(c); // error -> notdefined
 
 // var -> var variable is functional scope that means we cannot access outside that function.
-function print(){
-    var a = 10;
-    console.log(a);
-}
-print();
+// function print(){
+//     var a = 10;
+//     console.log(a);
+// }
+// print();
 // console.log(a); // error -> notdefined , we cannot access var variable outside that function.
+
+// variable shadowing
+// if inner score has the same variable name as outer scope, then priority goes to inner scope and it will print inner scope value
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+if(true){
+    var a = 100;
+    let b = 200;
+    const c = 300;
+    console.log(a); // 100
+    console.log(b); // 200
+    console.log(c); // 300
+}
+console.log(a); // 100
+console.log(b); // 20
+console.log(c); // 30
+
+
+
+
